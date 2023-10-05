8942f66a4d0558781501660780c252fd759647d8
kemal <kemal.hussein94@gmail.com>
2023-10-05 02:39:20 +0200

    AddedJWT-Authentication

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+use App\Entity\User;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setUsername("user");
+        $user->setEmail("user@gmail.com"); 
+        $user->setPassword($this->hasher->hashPassword($user, "user"));
+        $user->setCountry("Oromia");
+        $user->setCity("Finfine");
+        $user->setNumber("0922060394");
+
+        $admin = new User();
+        $admin->setUsername("admin");
+        $admin->setEmail("admin@gmail.com"); 
+        $admin->setPassword($this->hasher->hashPassword($admin, "admin"));
+        $admin->setCountry("France");
+        $admin->setCity("Lyon");
+        $admin->setNumber("0672581121");
+        $admin->setRoles(["ROLE_ADMIN"]);
+
+        $manager->persist($user);
+        $manager->persist($admin);
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Nft.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\NftRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NftRepository::class)]
+#[ApiResource]
+class Nft
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $creationDate = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $imageUrl = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?\DateTimeInterface
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(\DateTimeInterface $creationDate): static
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    public function getImageUrl(): ?string
+    {
+        return $this->imageUrl;
+    }
+
+    public function setImageUrl(string $imageUrl): static
+    {
+        $this->imageUrl = $imageUrl;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
